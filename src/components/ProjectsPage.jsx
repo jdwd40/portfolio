d4462690d658358a6cb5af261e25834db6a268c1
jdwd40 <jd@jdwd40.com>
2023-08-30 14:42:24 +0100

    added comming soon link to nc news live view link

--- src/components/ProjectsPage.jsx
+++ src/components/ProjectsPage.jsx
@@ -21,14 +21,14 @@ const projects = [
     title: 'NC News',
     description: 'NC News is a web application built with React, Chakra UI, and Vite. The app allows users to browse and interact with articles and comments.<br /> <ul style="padding-left: 2rem"><li>View a list of articles by topic</li><li>View an individual article</li><li>View an individual article\'s comments</li><li>Sort articles by date, comment_count, or votes</li><li>Post a new comment to an existing article (as a default user)</li></ul>',
     image: 'https://jdwd40.com/wp-content/uploads/2023/04/sabri-tuzcu-wunVFNvqhfE-unsplash.jpg',
-    liveLink: 'https://project1-live-link.com',
+    liveLink: '/comming-soon.html',
     githubLink: 'https://github.com/jdwd40/fe-vite-nc-news',
   },
   {
     title: 'Cypto Tracker',
     description: 'This application provides an easy way to track the current prices of popular cryptocurrencies and stay updated with the latest news articles related to crypto. Features include:<br /> <ul style="padding-left: 2rem"><li>View a list of the top 100 cryptocurrencies by market cap</li><li>View an individual cryptocurrency</li><li>View the latest news articles related to cryptocurrency</li><li>View an individual news article</li>',
     image: 'https://jdwd40.com/wp-content/uploads/2023/05/screenshot.png',
-    liveLink: 'https://jdc1.xyz',
+    liveLink: 'https://jwd1.xyz',
     githubLink: 'https://github.com/jdwd40/cryptoapp',
   },
   {
